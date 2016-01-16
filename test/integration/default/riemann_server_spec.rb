f2d505ff7c699cfe7d5a62bbd8f061d39d55e9a3
Conor Schaefer <conor.schaefer@gmail.com>
2016-01-15 19:03:20 -0500

    Adds more inspec tests for riemann config files

--- test/integration/default/riemann_server_spec.rb
+++ test/integration/default/riemann_server_spec.rb
@@ -10,4 +10,32 @@ end
 
 describe user('riemann') do
   it { should exist }
+  its('group') { should eq 'riemann' }
+  its('shell') { should eq '/bin/false' }
+  its('home') { should eq '/usr/share/riemann' }
+end
+
+describe file('/etc/logstash/conf.d') do
+  it { should_not exist }
+end
+
+riemann_directories = %w(
+  /usr/share/riemann
+  /etc/riemann
+  /var/log/riemann
+)
+riemann_directories.each do |dir|
+  describe file(dir) do
+    it { should exist }
+    it { should be_directory }
+  end
+end
+
+describe file('/etc/riemann/riemann.config') do
+  it { should exist }
+  its('owner') { should eq "root" }
+  its('group') { should eq "root" }
+  its('mode') { should eq 0644 }
+
+  its('content') { should match "(tcp-server {:host host :port 5555})" }
 end
